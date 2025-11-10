15f515624bf62a7cc543fd661a8763e950050b28
Diego740 <57465270+Diego740@users.noreply.github.com>
2025-11-10 04:37:19 +0100

    Update publicKeys.js

--- src/config/publicKeys.js
+++ src/config/publicKeys.js
@@ -1,10 +1,10 @@
 const SERVICE_ID =
-  import.meta.env.VITE_EMAILJS_SERVICE_ID || "service_ebbeaa3";
+  "service_ebbeaa3" || import.meta.env.VITE_EMAILJS_SERVICE_ID ;
 
 const TEMPLATE_ID =
-  import.meta.env.VITE_EMAILJS_TEMPLATE_ID || "template_z6uxp21";
+  "template_z6uxp21" || import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
 
 const PUBLIC_KEY =
-  import.meta.env.VITE_EMAILJS_PUBLIC_KEY || "iUFKPQ-LHiDbcpYkn";
+  "iUFKPQ-LHiDbcpYkn" || import.meta.env.VITE_EMAILJS_PUBLIC_KEY ;
 
 export { SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY };
