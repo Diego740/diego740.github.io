1580b3fe46fb62dbfdf872d444ef7007171dd4ff
Diego740 <57465270+Diego740@users.noreply.github.com>
2026-05-15 13:26:36 +0200

    Update writeups.js

--- src/config/writeups.js
+++ src/config/writeups.js
@@ -1,5 +1,13 @@
 export const writeups = [
   {
+    id: 'silentium',
+    title: 'Silentium',
+    os: 'Linux',
+    difficulty: 'Easy',
+    status: 'active',
+    date: '2026-04-15',
+  },
+  {
     id: 'updown',
     title: 'Updown',
     os: 'Linux',
@@ -16,5 +24,5 @@ export const writeups = [
     status: 'retired',
     date: '2026-05-15',
     file: '/writeups/Broker.md',
-  },
+  }
 ];
